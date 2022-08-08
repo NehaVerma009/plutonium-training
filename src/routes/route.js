224b3143f28478f7d1ccae10ddd4de919ae14328
NehaVerma009 <nehuverma009@gmail.com>
2022-08-08 23:51:04 +0530

    Assignment/get-api

--- src/routes/route.js
+++ src/routes/route.js
@@ -34,5 +34,85 @@ router.get('/student-details/:name', function(req, res){
     
     res.send('Dummy response')
 })
+//Array 
+
+let movies= ['Don', 'Rang De Basanti','Silent Sea','Chak De India']
+
+//     router.get('/movies', function(req, res){
+//        //print movies
+//         res.send (movies)
+
+
+// })
+
+//to get element from indexnumber
+//  router.get('/movies/:indexNumber', function (req, res){
+//     let requestParams = req.params.indexNumber
+    
+//     //res.send(movies[requestParams])
+// })
+
+//  router.get('/movies/:indexNumber',function(req,res){
+//     const movies=['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins'];
+//     let reqParams=req.params;
+//     let index=reqParams.indexNumber;
+//     if(index>=0 && index<=movies.length){
+//         res.send(movies[index])
+//     }
+//     else{
+//         res.send("Input Valid Index")
+//     }
+// })
+// //Problem 4
+
+// router.get('/films', function(req, res){
+//     let arr = [ {
+//         'id': 1,
+//         'name': 'The Shining'
+//        }, {
+//         'id': 2,
+//         'name': 'Incendies'
+//        }, {
+//         'id': 3,
+//         'name': 'Rang de Basanti'
+//        }, {
+//         'id': 4,
+//         'name': 'Finding Nemo'
+//        }]
+    
+
+//     res.send(arr)
+// })
+
+//Problem 5
+
+router.get('/films/:filmId', function(req, res){
+    let list= [ {
+        'id': 1,
+        'name': 'The Shining'
+       }, {
+        'id': 2,
+        'name': 'Incendies'
+       }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+       }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+       }]
+       let requestParams = req.params
+       let index=requestParams.filmId;
+if(filmId==id[i]){
+    res.send([i])
+
+
+}
+
+else{
+    res.send()
+
+}
+})
+
 
 module.exports = router;
