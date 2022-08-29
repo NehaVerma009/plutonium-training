bf6b78516b60510750c9b80bfd891ea0ec97be40
NehaVerma009 <nehuverma009@gmail.com>
2022-08-29 23:02:44 +0530

    Authorisation

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -11,12 +11,38 @@ let token = req.headers["x-Auth-token"]||req.headers["x-auth-token"];  //case se
   try{
     let decoded = jwt.verify(token,'functionup-plutonium-very-very-secret-key')
   }
+
 catch(error){
  //console.log(error)
     return res.send({msg:error.message})
   }
-  
+  try{
+    let decoded =  jwt.verify(token,'functionup-plutonium-very-very-secret-key')
+    let userToModify = req.params.userId
+    let userLoggedIn= decoded.userId
+    if(userToModify!=userLoggedIn){
+      return res.send({msg: " UnAuthorized User !"})
+    }else{userId}
+  }
+
+
+  catch (error){}
+
   next()
 }
+  
+
+  // let authorize = async function (req, res, next){
+  //   let decoded = jwt.verify(token,'functionup-plutonium-very-very-secret-key')
+  //   let userToModify = req.params.userId
+  //   let userLoggedIn= req.decoded.userId
+  //   if(userToModify!=userLoggedIn){
+  //     return res.send({msg: " UnAuthorized User !"})
+  //   }
+  //   console.log(token)
+  //   next()
+  // }
+
 
   module.exports.validating = validating
+  
