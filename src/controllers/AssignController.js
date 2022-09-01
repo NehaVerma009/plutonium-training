c64a70a4ae4594b18039c7c9468a689bef03b8fb
NehaVerma009 <nehuverma009@gmail.com>
2022-09-02 00:51:49 +0530

    memes

--- src/controllers/AssignController.js
+++ src/controllers/AssignController.js
@@ -26,4 +26,93 @@ let getByID = async function(req, res){
     }
 }
 
+
+let getWeather = async function (req,res){
+    try{
+        let place = req.query.q
+        let id = req.query.appid
+
+        console.log(place)
+        console.log(id)
+
+        var options = {
+            method: "get",
+            url: `http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${id}`
+        }
+
+        let result = await axios(options)
+        console.log(result.data)
+        res.status(200).send({msg:result.data})
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).send({msg:err.message})
+    
+    }
+
+}
+
+    let getTemp = async function(req,res){
+        try{
+        let place = req.query.q
+        let id = req.query.appid
+        console.log(place)
+        console.log(id)
+
+        var options = {
+            method: "get",
+            url: `http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${id}`
+        }
+
+        let result = await axios(options)
+        console.log(result.data.main)
+        res.status(200).send({msg:result.data.main})
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).send({msg:err.message})
+    
+    }
+
+    }
+
+
+    let citiesWithTemp = async function (req, res) {
+        try {
+            let array = ["Bengaluru","Mumbai", "Delhi", "Kolkata", "Chennai", "London", "Moscow"]
+            let newarr = []
+            for (i = 0; i < array.length; i++) {
+                let obj = { city: array[i] }
+                console.log(array[i])
+                let result = await axios.get( `http://api.openweathermap.org/data/2.5/weather?q=${array[i]}&appid=dc0b488426e2831fcc56a0f8a5e01994`)
+                 console.log(result.data.main.temp)
+                 obj[array[i]] =result.data.main.temp
+                newarr.push(obj)
+              }
+              
+              res.status(200).send({ msg: newarr })
+        } 
+        catch (err) { 
+            console.log(err)
+            res.status(500).send({ msg: err.message })
+        }
+       }
+
+    // let memeList = async function (req, res){
+    //     try{
+
+    //     }
+    //     catch(err){
+    //         console.log(err)
+    //         res.status(500).send({msg:err.message})
+    //     }
+    // }
+
+
+
+
+
 module.exports.getByID = getByID
+module.exports.getWeather = getWeather
+module.exports.getTemp = getTemp
+module.exports.citiesWithTemp = citiesWithTemp
--- src/routes/route.js
+++ src/routes/route.js
@@ -15,6 +15,9 @@ router.get("/cowin/districtsInState/:stateId",CowinController.getDistricts)
 router.get("/cowin/getByPin", CowinController.getByPin)
 router.post("/cowin/getOtp", CowinController.getOtp)
 router.get("/cowin/getById",AssignController.getByID)
+router.get("/weather",AssignController.getWeather)
+router.get("/temperature",AssignController.getTemp)
+router.get("/citiesWithTemp",AssignController.citiesWithTemp)
 
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
 
